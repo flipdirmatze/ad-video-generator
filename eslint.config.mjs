909334ecd7d43ad2c4a730c7ec238bf219f21c31
Your Name <you@example.com>
2025-03-07 22:05:32 +0100

    Add ESLint config to disable unused vars rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,39 @@
-import { dirname } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import nextPlugin from '@next/eslint-plugin-next';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __path.dirname = path.dirname(__filename);
+
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __path.dirname,
+  recommendedConfig: eslint.configs.recommended,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'plugin:@typescript-eslint/recommended'
+    ],
+  }),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-unused-vars': 'off'
+    }
+  },
+  {
+    plugins: {
+      '@next/next': nextPlugin
+    },
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off'
+    }
+  }
 ];
-
-export default eslintConfig;
