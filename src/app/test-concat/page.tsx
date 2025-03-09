9b84009a34a510bb25f2db8b62de67b323b9518c
Your Name <you@example.com>
2025-03-09 15:41:04 +0100

    Entferne ungenutzte test-concat Seite

--- src/app/test-concat/page.tsx
+++
@@ -1,2 +0,0 @@
-// Diese Datei kann vollständig entfernt werden, da sie nur zum Testen dient.
-// Stattdessen sollte die eigentliche Concat-Funktionalität in src/app/concat/page.tsx verbessert werden. 
